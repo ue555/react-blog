240bd2538a878b3f1d1252a6e4ca626cd5e25a57
UKouji555 <zerozero53205320.dev@gmail.com>
2025-08-11 11:57:37 +0900

    fix: GitHub Pages settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tech-blog/',
-  server: {
-    port: 3000,
-    open: true,
-  },
+  base: '/react-blog/',
   build: {
     outDir: 'dist',
     sourcemap: false,
